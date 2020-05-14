4095b60ee8026452dd611d51e4b88859228c6eca
pradeepcsu <pradeepcsu@gmail.com>
2020-05-14 15:10:36 -0500

    updated test.js
    completed required code for assignment please let me know where I can improve my scripting skills, I would be happy to.

--- test.js
+++ test.js
@@ -19,27 +19,42 @@ describe('JS-Selenium-Exercise', function () {
             assert.equal(title, "Google");
 
             // Check that the google logo is visible
+            const googleLogo = await driver.findElement(By.xpath("//img[@id='hplogo']"));
+            expect(googleLogo.isDisplayed()).toEqual(true);
+            // driver.findElement(webdriver.By.xpath("//img[@id='hplogo']"))
 
 
             // Check there are two options present: "Google Search" and "I'm Feeling Lucky"
-
-
+            const googleSearchBtn = await driver.findElement(By.xpath("//input[@name='btnK']"));
+            const imFeelingLuckyBtn = await driver.findElement(By.xpath("//span[contains(text(),'Feeling Trendy')]"));
+            expect(googleSearchBtn.isDisplayed()).toEqual(true);
+            expect(imFeelingLuckyBtn.isDisplayed()).toEqual(true);
+            
             // Enter text "PayPal" and click "I'm Feeling Lucky"
-
+            driver.findElement(By.xpath("//input[@name='q']")).sendKeys("PayPal");
+            (await driver.findElement(By.xpath("//span[contains(text(),'Feeling Trendy')]"))).click();
 
             // Check the url is now "https://www.paypal.com/"
-
+            let currentURL = await driver.getCurrentUrl();
+            assert.equal(currentURL, "https://www.paypal.com/");
 
             // Click "Sitemap"
-
+            (await driver.findElement(By.xpath("//a[text()='Sitemap']"))).click();
 
             // Check the url is now "https://www.paypal.com/us/webapps/mpp/full-sitemap"
-
-
+            let currentURL = await driver.getCurrentUrl();
+            assert.equal(currentURL, "https://www.paypal.com/us/webapps/mpp/full-sitemap");
 
             // Store all of the links on this page into a list and then print them all to system.out
-
-
+           
+            driver.findElements(By.tagName("a")).then(function(elements){
+                elements.forEach(function (element) {
+                    element.getText().then(function(text){
+                        console.log(text);
+                    });
+                });
+            });
+            
             await driver.quit();
         })
     })
